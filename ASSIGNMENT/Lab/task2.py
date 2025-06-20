40613ef841feffc1e5d9c36d2311e27a4e04299a
Niyati03-ship-it <niyatigajjar03@gmail.com>
2025-06-20 12:47:16 +0530

    Add files via upload

---
+++ ASSIGNMENT/Lab/task2.py
@@ -0,0 +1,18 @@
+# This program displays student information
+# Following PEP 8: indentation, comments, and variable naming
+
+# Assign values to variables
+student_name = "Rahul Mehta"
+student_age = 17
+student_class = "12th Grade"
+
+# Print the student details
+print("Student Information:")
+print("Name :", student_name)
+print("Age  :", student_age)
+print("Class:", student_class)
+
+
+
+
+
