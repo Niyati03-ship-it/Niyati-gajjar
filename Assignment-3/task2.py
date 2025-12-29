68220a397b01689d4ec7c010dd195cd63032a337
Niyati03-ship-it <niyatigajjar03@gmail.com>
2025-12-29 14:02:25 +0530

    Delete Assignment-3 directory

--- Assignment-3/task2.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program to read a name and age from the user and print a formatted output.
-
-
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-
-print(f"Name: {name}, Age: {age}")
-
-
